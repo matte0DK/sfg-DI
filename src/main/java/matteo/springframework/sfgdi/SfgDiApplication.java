bf5a84d4ffa6457d8035a0480b9c2e5d2ef8a345
matte0DK <72343291+matte0DK@users.noreply.github.com>
2021-05-10 14:58:13 +0200

    added singleton en prototype bean examples

--- src/main/java/matteo/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/matteo/springframework/sfgdi/SfgDiApplication.java
@@ -1,6 +1,8 @@
 package matteo.springframework.sfgdi;
 
 import matteo.springframework.sfgdi.controllers.*;
+import matteo.springframework.sfgdi.services.PrototypeBean;
+import matteo.springframework.sfgdi.services.SingletonBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -35,6 +37,17 @@ public class SfgDiApplication {
         System.out.println("------ Constructor");
         ConstructorInjectionController constructorInjectionController = (ConstructorInjectionController) context.getBean("constructorInjectionController");
         System.out.println(constructorInjectionController.getGreeting());
+
+        System.out.println("------ Bean scopes");
+        SingletonBean singletonBean = context.getBean(SingletonBean.class);
+        System.out.println(singletonBean.getMyScope());
+        SingletonBean singletonBean1 = context.getBean(SingletonBean.class);
+        System.out.println(singletonBean1.getMyScope());
+
+        PrototypeBean prototypeBean = context.getBean(PrototypeBean.class);
+        System.out.println(prototypeBean.getMyScope());
+        PrototypeBean prototypeBean1 = context.getBean(PrototypeBean.class);
+        System.out.println(prototypeBean1.getMyScope());
     }
 
 }
